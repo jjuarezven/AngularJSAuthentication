2d9d382abb43518dd24fd5704f657701c7a4700c
JJuarez <jose.juarez@yuxipacific.com>
2016-04-20 14:38:57 -0500

    Part 1 of post's series

---
+++ AngularJSAuthentication.API/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Owin.Cors;
+
+//http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/
+[assembly: OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
+namespace AngularJSAuthentication.API
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			HttpConfiguration config = new HttpConfiguration();
+			ConfigureOAuth(app);
+			WebApiConfig.Register(config);
+			app.UseCors(CorsOptions.AllowAll);
+			app.UseWebApi(config);
+		}
+
+		private void ConfigureOAuth(IAppBuilder app)
+		{
+			OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+			{
+				AllowInsecureHttp = true,
+				TokenEndpointPath = new PathString("/token"),
+				AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+				Provider = new SimpleAuthorizationServerProvider()
+			};
+
+			// Token Generation
+			app.UseOAuthAuthorizationServer(OAuthServerOptions);
+			app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+		}
+	}
+}
