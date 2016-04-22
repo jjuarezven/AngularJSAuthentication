719b1bfa41dae82c0b819af38ae4db7db4b1f868
JJuarez <jose.juarez@yuxipacific.com>
2016-04-22 11:47:17 -0500

    Part 2 working locally

--- AngularJSAuthentication.Web/app/services/AuthService.js
+++ AngularJSAuthentication.Web/app/services/AuthService.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
 app.factory('authService', ['$http', '$q', 'localStorageService', function ($http, $q, localStorageService) {
 
-	var serviceBase = 'http://ngauthenticationapi.azurewebsites.net/';
+	var serviceBase = 'http://localhost:49909/';//'http://ngauthenticationapi.azurewebsites.net/';
 	var authServiceFactory = {};
 
 	var _authentication = {
